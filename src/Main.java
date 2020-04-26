64e602df6114748661b4d5fc86001a33964bb900
Rian <rdkilgore@gmail.com>
2020-04-26 13:06:10 -0700

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        TechnicalWriter technicalWriter = new TechnicalWriter("Mark", 50000, 4, 2);
-        System.out.println(technicalWriter.toString());
-        System.out.println();
-        Engineer engineer = new Engineer("Natashia", 50000, 7, 2);
-        System.out.println(engineer.toString());
-        System.out.println();
-        ProductManager productManager = new ProductManager("Carlos", 50000, 8, 5);
-        System.out.println(productManager.toString());
-        System.out.println();
+        FacilityManager boss = new FacilityManager();
+        boss.setTechnicalWriter("Mark", 50000, 4, 2);
+        boss.setProductManger("April", 50000, 4, 2);
+        boss.setEngineer("Mae", 50000, 4, 2);
+        boss.printAllEmployees();
     }
 }
+       // companyX.add(new Employee("June", 50000, 4, 2));
+        ;
+//        System.out.println();
+//        Engineer engineer = new Engineer("Natashia", 50000, 7, 2);
+//        System.out.println(engineer.toString());
+//        System.out.println();
+//        ProductManager productManager = new ProductManager("Carlos", 50000, 8, 5);
+//        System.out.println(productManager.toString());
+//        System.out.println();
+
+
